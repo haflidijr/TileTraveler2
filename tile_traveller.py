c5240d9fc7ba2a059b4d18d2c055199c0eabb49d
Eggert <eggertgeir@gmail.com>
2020-10-08 09:53:17 +0000

    coins function til

--- tile_traveller.py
+++ tile_traveller.py
@@ -17,6 +17,23 @@ def move(direction, col, row):
         col -= 1
     return(col, row)    
 
+def coins(total_coins):
+    lever = str(input("Pull a lever (y/n): "))
+    
+    while lever != "y" or lever != "n":
+        
+        if lever == 'y':
+            total_coins += 1
+            print("You received 1 coin, your total is now", total_coins + ".")
+            break
+        elif lever == 'n':
+            break
+        else:
+            print("Invalid Input")
+            lever = str(input("Pull a lever (y/n): "))
+            continue
+        return total_coins
+    
 def is_victory(col, row):
     ''' Return true if player is in the victory cell '''
     return col == 3 and row == 1 # (3,1)
@@ -76,6 +93,7 @@ def play_one_move(col, row, valid_directions):
 victory = False
 row = 1
 col = 1
+total_coins = 0
 
 while not victory:
     valid_directions = find_directions(col, row)
